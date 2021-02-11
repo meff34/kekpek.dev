f77c1856d315689033b50a921d32750812dc779e
i.gorbunov <meff34@gmail.com>
2021-02-11 19:50:11 +0300

    reduce og banner size

--- src/features/seo/index.tsx
+++ src/features/seo/index.tsx
@@ -21,7 +21,7 @@ const useQuery = () =>
         }
         file(base: { eq: "mage.png" }) {
           childImageSharp {
-            fixed {
+            fixed(width: 384) {
               src
             }
           }
